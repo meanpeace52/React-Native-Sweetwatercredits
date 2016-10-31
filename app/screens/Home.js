4dffab15df5a2443eab1348eb11e79662aedcff1
solar-dev23 <solar.dev023@gmail.com>
2016-10-31 11:22:04 -0400

    Clean up file tree and refactor code in screens

--- app/screens/Home.js
+++ app/screens/Home.js
@@ -1,16 +1,117 @@
 'use strict'
 
 import React, { Component } from 'react'
-import { Image, StyleSheet, Text, TouchableHighlight, View, TabBarIOS } from 'react-native'
+import { Image, StyleSheet, Text, TouchableHighlight, View } from 'react-native'
 
-// import Icon from 'react-native-vector-icons/Ionicons'
+// Vendor Packages
+import * as _ from 'lodash'
+
+// Custom Components
+import StatusBarBackground from '../components/StatusBarBackground'
+import ViewContainer from '../components/ViewContainer'
 
 class Home extends Component {
-  render() {
-    return (
-      
+  _navigate (routeName) {
+    this.props.navigator.push({
+      name: routeName
+    })
+  }
 
+  render () {
+    return (
+      <ViewContainer>
+        <StatusBarBackground />
+        <View style={styles.ImageContainer}>
+          <Image
+          style={styles.SplashImage}
+          source={require('../assets/images/sweetwater_white_logo_small.jpg')}/>
+        </View>
+
+        <TouchableHighlight
+        style={styles.button}
+        onPress={ () => this._navigate('project') }>
+          <Text style={styles.buttonText}>
+            New Project
+          </Text>
+        </TouchableHighlight>
+
+        <Text style={styles.contentText}>
+          {_.upperCase('Note')}
+          : Registration is not required to use our calculator, but it is helpful to save your progress, edit previous calculations, and share results with colleagues.
+        </Text>
+
+        <TouchableHighlight
+        style={styles.bottomButton}
+        onPress={ () => this._navigate('signin') }>
+          <Text style={styles.buttonText}>
+            Sign In
+          </Text>
+        </TouchableHighlight>
+
+        <TouchableHighlight
+        style={styles.bottomButton}
+        onPress={ () => this._navigate('register') }>
+          <Text style={styles.buttonText}>
+            Register
+          </Text>
+        </TouchableHighlight>
+
+        <TouchableHighlight
+        style={styles.bottomButton}
+        onPress={ () => this._navigate('about') }>
+          <Text style={styles.buttonText}>
+            About
+          </Text>
+        </TouchableHighlight>
+
+      </ViewContainer>
     )
   }
 }
+
+const styles = StyleSheet.create({
+  button: {
+    alignSelf: 'center',
+    alignItems: 'center',
+    backgroundColor: 'mistyrose',
+    width: 325,
+    height: 40
+  },
+  bottomButton: {
+    alignSelf: 'center',
+    alignItems: 'center',
+    backgroundColor: 'mistyrose',
+    width: 325,
+    height: 40,
+    marginBottom: 10
+  },
+  buttonText: {
+    fontSize: 20,
+    color: 'black',
+    marginTop: 7
+  },
+  ImageContainer: {
+    marginTop: 25,
+    marginLeft: 25,
+    marginBottom: 40
+  },
+  projectTitle: {
+    justifyContent: 'center',
+    alignSelf: 'center',
+    marginTop: 30,
+    fontSize: 46
+  },
+  SplashImage: {
+    width: 150,
+    height: 92
+  },
+  contentText: {
+    marginTop: 25,
+    marginBottom: 25,
+    marginRight: 25,
+    marginLeft: 25,
+    fontSize: 24
+  }
+})
+
 module.exports = Home
